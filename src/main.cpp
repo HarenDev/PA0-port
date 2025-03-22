9bb20e1436f4dd55abe16286b5d2789ea91f98b7
HarenDev <157764758+HarenDev@users.noreply.github.com>
2025-03-22 04:16:55 -0400

    Attempt to prevent white flash

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,10 @@ int main ()
 	// Create the window and OpenGL context & tell the window to use vsync and work on high DPI displays
 	raylib::Window window(1024, 724, "Hello Students!!!", FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
+	BeginDrawing();
+	ClearBackground(GRAY);
+	EndDrawing();
+
 	// Create program (if we were loading images & fonts, 
 	//then you need to make sure that you load() them after window creation)
 	Program program;
